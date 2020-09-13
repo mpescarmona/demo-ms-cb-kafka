0492975eb48d1cb06dd507035741d9b8413f88f5
Mario Pescarmona <mpescarmona@gmail.com>
2020-09-12 22:58:06 -0300

    Initial Commit

---
+++ src/main/java/com/mpescarmona/demo/DemoMsCbKafkaApplication.java
@@ -0,0 +1,12 @@
+package com.mpescarmona.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMsCbKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMsCbKafkaApplication.class, args);
+    }
+}
