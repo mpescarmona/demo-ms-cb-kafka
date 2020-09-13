74f4bcc06d3b03af4ba6b8d4576bc24cfa0e1047
Mario Pescarmona <mpescarmona@gmail.com>
2020-09-13 01:30:07 -0300

    Adding status controller

---
+++ src/main/java/com/mpescarmona/demo/controller/StatusController.java
@@ -0,0 +1,39 @@
+package com.mpescarmona.demo.controller;
+
+import com.mpescarmona.demo.dto.StatusDto;
+import com.mpescarmona.demo.service.failsimulatormongo.FailSimulatorService;
+import com.mpescarmona.demo.service.rockband.RockBandLikesService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controlador que expone el estado del microservicio
+ */
+@Api(value = "Status controller")
+@Slf4j
+@Validated
+@RestController
+@RequestMapping("/demo")
+@RequiredArgsConstructor
+public class StatusController {
+    private final RockBandLikesService rockBandLikesService;
+    private final FailSimulatorService failSimulatorService;
+
+    @ApiOperation("Gets the microservice status")
+    @GetMapping(value = "/status")
+    public ResponseEntity<StatusDto> getStatus() {
+
+        StatusDto statusDto = StatusDto.builder()
+                .likeEvents(rockBandLikesService.getRockBandLikesCount())
+                .delay(failSimulatorService.getDelayInSeconds())
+                .build();
+        return ResponseEntity.ok(statusDto);
+    }
+}
---
+++ src/test/groovy/com/mpescarmona/demo/controller/StatusControllerTest.groovy
@@ -0,0 +1,37 @@
+package com.mpescarmona.demo.controller
+
+import com.mpescarmona.demo.dto.StatusDto
+import com.mpescarmona.demo.service.failsimulatormongo.FailSimulatorService
+import com.mpescarmona.demo.service.rockband.RockBandLikesService
+import org.springframework.http.ResponseEntity
+import spock.lang.Specification
+
+class StatusControllerTest extends Specification {
+    RockBandLikesService rockBandLikesService
+    FailSimulatorService failSimulatorService
+    StatusController statusController
+
+    def setup() {
+        rockBandLikesService = Mock(RockBandLikesService)
+        failSimulatorService = Mock(FailSimulatorService)
+        statusController = new StatusController(rockBandLikesService, failSimulatorService)
+    }
+
+    def "GetStatus should return the status of rock band and fail services"() {
+        given:
+        def delay = 0
+        def likes = 1500
+        def expectedResult = StatusDto.builder()
+                .delay(delay)
+                .likeEvents(likes)
+                .build()
+        rockBandLikesService.getRockBandLikesCount() >> likes
+        failSimulatorService.getDelayInSeconds() >> delay
+
+        when:
+        def result = statusController.getStatus()
+
+        then:
+        result == ResponseEntity.ok(expectedResult)
+    }
+}
